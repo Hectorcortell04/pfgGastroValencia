97ff7b1a4edbe689bf6a54a0788708bff952a3ae
Hectorcortell04 <Hectorcortell@gmail.com>
2025-03-23 15:47:26 +0100

    Modification RestaurantMapper to use the DTO.

--- src/main/java/com/example/gastroValenciaApi/mappers/RestaurantMapper.java
+++ src/main/java/com/example/gastroValenciaApi/mappers/RestaurantMapper.java
@@ -1,4 +1,15 @@
 package com.example.gastroValenciaApi.mappers;
 
-public class RestaurantMapper {
+
+import com.example.gastroValenciaApi.dtos.RestaurantDTO;
+import com.example.gastroValenciaApi.models.RestaurantModel;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface RestaurantMapper {
+    RestaurantDTO toDTO(RestaurantModel restaurant);
+
+    @InheritInverseConfiguration
+    RestaurantModel toEntity(RestaurantDTO dto);
 }
