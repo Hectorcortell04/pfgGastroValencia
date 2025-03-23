a5c09ac6f093026b9b0acbfb60856e9cf3140dbd
Hectorcortell04 <Hectorcortell@gmail.com>
2025-03-23 15:47:06 +0100

    Modification UserMapper to use de DTO.

--- src/main/java/com/example/gastroValenciaApi/mappers/UserMapper.java
+++ src/main/java/com/example/gastroValenciaApi/mappers/UserMapper.java
@@ -1,4 +1,16 @@
 package com.example.gastroValenciaApi.mappers;
 
-public class UserMapper {
+
+import com.example.gastroValenciaApi.dtos.UserDTO;
+import com.example.gastroValenciaApi.models.UserModel;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserDTO toDTO(UserModel model);
+
+
+    @InheritInverseConfiguration
+    UserModel toEntity(UserDTO dto);
 }
